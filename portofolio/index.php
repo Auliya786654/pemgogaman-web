d65eaf86371a75a16c268517df6114c8da560c71
Auliya786654 <137489114+Auliya786654@users.noreply.github.com>
2023-06-24 14:27:36 +0700

    Add files via upload

---
+++ portofolio/index.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width="device-width,initial-scales=1" s/>
+<title>portofolio</title>
+</head>
+<body>
+<p>Auliya Fitra Sabila</p>
+<p>
+<p>Saya Tinggal di Purwodadi, Kec. Tonjong, Kab. Brebes. </p>
+<p>Saya sebagai mahasiswa universitas Peradaban.
+</p>
+</body>
+<ol>
+<li>Posisi yang diusulkan   : Tenaga Ahli sistem Geografis</li>
+<li>Nama Perusahaan         : PT.Patita Galxy</li>
+<li>Nama Personil           : Auliya Fita Sabila</li>
+<li>Tempat/Tanggal Lahir    : Brebes 24 September 2002</li>
+<li>Pendidikan              :
+<ol type="a">
+<li>Sarjana S1 Informatika</li>
+</ol>
+<li>Pendidikan Non Formal :</li>
+<ul type="disc">
+    <li>MAPABA</li>
+    <li>Pelatihan Desain Grafis</li>
+</ul>
+<li>Penguasaan Pendidikan :</li>
+<ol type="a">
+<li>Bahasa Indonesia : Baik</li>
+<li>Bahasa Inggris   : Baik</li>
+<li>Bahasa Ngapak    : Sangat Baik</li>
+</ol>
+<li>Pengalam Kerja   :</li>
+<ol type="a">
+    <li>Nama Pekerjaan : Ibu Rumah Tangga</li> 
+    <li>Lokasi         : Rumah Saya Sendiri</li>
+</html>
